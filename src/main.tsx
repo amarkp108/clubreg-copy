1a9a5fbfc3066624166185a0ef74fe24e9437fb1
amar kumar pandit <amarkp108@gmail.com>
2026-04-16 15:59:36 +0530

    Submission table

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
+import { ClubSelectionForm } from "./components/ClubSelectionForm";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
@@ -9,11 +8,10 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
-const router = getRouter();
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ClubSelectionForm />
   </React.StrictMode>,
 );
